77547ebc5e74dafc3b559dd87039c761a4af56dd
Alyani Mamad <alyanimamadbhai@gmail.com>
2023-05-28 21:36:34 +0530

    Lab 5 prog 2&3 done

---
+++ Day-40/l5p2.c
@@ -0,0 +1,46 @@
+/*An electric power distribution company charges its domestic consumers as
+follows:
+Consumption Units Rate of Charge
+1-100 Rs. 0.75 per unit
+101-300 Rs. 75 plus Rs. 1.00 per unit excess of 100
+301-500 Rs. 275 plus Rs. 1.50 per unit excess of 300
+500 above Rs. 575 plus Rs. 1.75 per unit excess of 500
+Write a program that reads customer number & power consumed and print the
+amount to be paid by the customer. Note that output should be well formatted
+with meaningful calculation displayed as invoice.*/
+
+
+#include <stdio.h>
+
+int main(){
+
+    int cno,pc;
+    float rate;
+
+    printf("\nEnter Customer Number : ");
+    scanf("%d",&cno);
+
+    printf("\nEnter Power Consumed : ");
+    scanf("%d",&pc);
+
+    if(pc>=0 && pc<=100){
+        rate=.75*pc;
+    }else if(pc>=101 && pc<300){
+         rate=1.5*pc;   
+    }else if(pc>=301 && pc<500){
+         rate=1.50*pc;
+    }else if(pc>=501){
+         rate=1.75*pc;
+    }else{
+        printf("\nPlease proper input");
+    }
+
+    printf("\n\tFinal Bill\n");
+    printf("----------------------");
+    printf("\nCustomer No    :%d",cno);
+    printf("\nPower Consumed :%d",pc);
+    printf("\n----------------------");
+    printf("\nTotal Amount   :%.2f",rate);
+
+    return 0;
+}
