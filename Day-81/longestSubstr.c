93c47c2abb723ece11759678fc5e23fe46a86a80
Alyani Mamad <97268754+AlyaniMamad@users.noreply.github.com>
2023-07-04 14:34:24 +0530

    Added one array question

---
+++ Day-81/longestSubstr.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+
+    char str[100];
+    int cnt=1;
+
+    printf("\nEnter String : ");
+    gets(str);
+
+    int n=strlen(str)-1;
+
+    for(int i=0;i<n;i++){
+        for(int j=i+1;j<n;j++){
+            if(str[i]!=str[j]){
+                cnt++;
+                
+            }
+        }
+    }
+
+    printf("%d",cnt);
+
+    return 0;
+}
